ed08f3b79b122483d1b6ac25b283b2b5911e1f14
Ben McLaren <benmclaren@live.co.uk>
2020-06-25 14:01:37 +0100

    implemented anime.js

---
+++ main.js
@@ -0,0 +1,10 @@
+anime({
+  // normal css selector
+  targets: '#tunnel circle',
+  // here we scale by 20%
+  scale: 1.2,
+  // returns it to beginning
+  direction: 'alternate',
+  // loops
+  loop: true
+});
