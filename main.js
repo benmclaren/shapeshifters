0dc716d75ebfb72610260054f5f74d9e805e66a4
Ben McLaren <benmclaren@live.co.uk>
2020-06-26 14:49:45 +0100

    frst animation working

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ anime({
   // normal css selector
   targets: '#tunnel circle',
   // here we scale by 20%
-  scale: 1.2,
+  scale: 1.1,
   // returns it to beginning
   direction: 'alternate',
   // loops
-  loop: true
+  loop: true,
+  duration: 250,
+  // easings allow us to add more character to animations
+  easing: 'easeInOutCubic'
 });
